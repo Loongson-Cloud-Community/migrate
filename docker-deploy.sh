4cefbd3aae8dbc0fbe2432fdcecb79331f11b6a8
yzewei <yangzewei@loongson.cn>
2023-09-26 16:14:52 +0800

    update docker-deploy.sh

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker build  --build-arg VERSION="$TRAVIS_TAG" . -t cr.loongnix.cn/library/migrate 
+docker build  \
+	--build-arg http_proxy="$http_proxy" \
+        --build-arg https_proxy="$https_proxy" \
+	--build-arg VERSION="4.15.1" . \
+	-t cr.loongnix.cn/library/migrate 
