f61eb745e0c1d4a04af4436831d1753cba89a1bd
yzewei <yangzewei@loongson.cn>
2023-09-26 16:30:58 +0800

    update

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -4,4 +4,4 @@ docker build  \
 	--build-arg http_proxy="$http_proxy" \
         --build-arg https_proxy="$https_proxy" \
 	--build-arg VERSION="4.15.1" . \
-	-t cr.loongnix.cn/library/migrate 
+	-t cr.loongnix.cn/library/migrate:4.15.1 
